e91e9733e1ae2de535017c565f1b8f98e5770033
Carlos <carlos.av.amorim@gmail.com>
2022-07-21 21:35:19 -0300

    Style: Add register interface

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
-import { Home } from './src/screens/Home';
 import { NativeBaseProvider, StatusBar } from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import { THEME } from './src/styles/theme'
+
+import { Register } from './src/screens/Register';
 import { Loading } from './src/components/Loading';
 
 export default function App() {
@@ -12,7 +13,7 @@ export default function App() {
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar barStyle='light-content' backgroundColor="transparent" translucent />
-      {fontsLoaded ? <Home /> : <Loading />}
+      {fontsLoaded ? <Register /> : <Loading />}
     </NativeBaseProvider>
   );
 }
