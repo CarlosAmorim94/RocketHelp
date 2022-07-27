169ac2560822b6ca5612f9b875d5bd95b52cd229
Carlos <carlos.av.amorim@gmail.com>
2022-07-26 21:36:53 -0300

    Feat: Add login with Firebase

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,33 @@
 import { NavigationContainer } from '@react-navigation/native'
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth'
 
 import { SignIn } from '../screens/Signin';
 import { AppRoutes } from './app.routes';
+import { useEffect, useState } from 'react';
+import { Loading } from '../components/Loading';
 
 export function Routes() {
+
+  const [loading, setIsLoading] = useState(true)
+  const [user, setUser] = useState<FirebaseAuthTypes.User>()
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged((response) => {
+      setUser(response)
+      setIsLoading(false)
+    })
+
+    return subscriber
+  }, [])
+
+  if (loading) {
+    return <Loading />
+  }
+
+
   return (
     <NavigationContainer>
-      <AppRoutes />
+      {user ? <AppRoutes /> : <SignIn />}
     </NavigationContainer>
   );
 }
--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+import auth from '@react-native-firebase/auth'
 import { VStack, Heading, Icon, useTheme } from "native-base"
 import { Envelope, Key } from 'phosphor-react-native'
 
@@ -5,17 +7,44 @@ import Logo from '../assets/logo_primary.svg'
 import { Input } from "../components/Input"
 
 import { Button } from "../components/Button"
-import { useState } from "react"
+import { Alert } from "react-native"
 
 export const SignIn = () => {
 
   const { colors } = useTheme()
 
-  const [email, setEmail] = useState('teste@gmail.com')
+  const [isLoading, setIsLoading] = useState(false)
+  const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
   function handleSignIn() {
-    console.log(email, password)
+    if (!email || !password) {
+      return Alert.alert('Login', 'Informe o e-mail e senha')
+    }
+
+    setIsLoading(true)
+
+    auth().signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        console.log(error.code)
+        setIsLoading(false)
+
+        if (error.code === 'auth/user-not-found') {
+          return Alert.alert('Login', 'Usuário não cadastrado')
+        }
+
+        if (error.code === 'auth/invalid-email') {
+          return Alert.alert('Login', 'E-mail ou senha inválidos')
+        }
+
+        if (error.code === 'auth/wrong-password') {
+          return Alert.alert('Login', 'E-mail ou senha inválidos')
+        }
+
+        return Alert.alert('Login', 'Não foi possivel acessar, contatar administrador/responsável')
+
+      })
+
   }
 
   return (
@@ -41,7 +70,12 @@ export const SignIn = () => {
         onChangeText={setPassword}
       />
 
-      <Button title="Entrar" w="full" onPress={handleSignIn} />
+      <Button
+        title="Entrar"
+        w="full"
+        onPress={handleSignIn}
+        isLoading={isLoading}
+      />
 
     </VStack>
   )
